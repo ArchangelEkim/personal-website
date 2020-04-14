cde6fd6fe3c9047fca135dfa705e707a48fdd9f1
Michael <mickey278mouse@iinet.net.au>
2020-04-14 14:10:12 +1000

    Added https support

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
 const express = require('express')
-const passport = require('passport')
+
+const hostname = 'aosora.ddns.net'
+const httpPort = 3000
+const httpsPort = 3001
+
+const httpsOptions = {
+    cert: fs.readFileSync('./ssl/domain-crt.crt'),
+    key: fs.readFileSync('./ssl/domain-key.key')
+}
+
 const app = express()
+const httpServer = http.createServer(app)
+const httpsServer = https.createServer(httpsOptions, app)
 
-const port = 3000
+app.use((req, res, next) => {
+    if(req.protocol == 'http'){
+        res.redirect(301, `https://${req.headers.host}${req.url}`);
+    }
+    next();
+});
 
-app.use('/', express.static('public'))
-app.use('/dev', express.static('development'))
+app.use('/', express.static('public', { extensions: ['html','htm']}))
 
-app.listen(port, () => console.log(`Web application listening at https:\\localhost:${port}`))
+httpServer.listen(httpPort, () => console.log(`Http server is listening at ${hostname}:${httpPort}`))
+httpsServer.listen(httpsPort, () => console.log(`Https server is listening at ${hostname}:${httpsPort}`))
